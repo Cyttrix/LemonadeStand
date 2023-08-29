16566a251aef104603fa73d51c5ab617dc4ba2ba
Ari Meyer <ari.f.meyer@gmail.com>
2023-08-29 11:21:29 -0500

    Uploading LemonadeStand project

---
+++ LemonadeStand/Customer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    internal class Customer
+    {
+        //member variables
+        public string WeatherPref;
+        public int MoneyPref;
+        private Random rnd;
+        //constructor
+        public Customer()
+        {
+            rnd = new Random();
+            WeatherPref = GetWeatherPref();
+            MoneyPref = GetMoneyPref();
+        }
+
+        //memeber methods
+        public string GetWeatherPref()
+        {
+            int randNumber = rnd.Next(1,6);
+            
+            if(randNumber == 1)
+            {
+                string preference = "Sunny";
+                return preference;
+            }
+            else if(randNumber == 2)
+            {
+                string preference = "Cloudy";
+                return preference;
+            }
+            else if (randNumber == 3)
+            {
+                string preference = "Windy";
+                return preference;
+            }
+            else if (randNumber == 4)
+            {
+                string preference = "Rainy";
+                return preference;
+            }
+            else
+            {
+                string preference = "Stormy";
+                return "Stormy";
+            }
+         
+            
+            
+        }
+
+        public int GetMoneyPref()
+        {
+            int moneyPref = rnd.Next(1, 6);
+            
+            
+            return moneyPref;
+        }
+        public bool BuyLemonade(int priceOfCup, string Weather)
+        {
+           if (MoneyPref <= priceOfCup && WeatherPref == Weather)
+            {
+                return true;
+
+            }
+           else
+            {
+                return false;
+            }
+
+            
+        }
+       
+
+    }
+}
---
+++ LemonadeStand/Weather.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+
+    public class Weather
+    {
+        private Random random;
+        public string DayWeather;
+
+        public Weather()
+        {
+            random = new Random();
+            DayWeather = GetWeather();
+        }
+
+        public string GetWeather()
+        {
+            int rndnumb = random.Next(1, 6);
+            
+            if (rndnumb == 1)
+            {
+                string weather = "Sunny";
+                return weather;
+            }
+            if (rndnumb == 2)
+            {
+                string weather = "Cloudy";
+                return weather;
+            }
+            if (rndnumb == 3)
+            {
+                string weather = "Windy";
+                return weather;
+            }
+            if (rndnumb == 4)
+            {
+                string weather = "Rainy";
+                return weather;
+            }
+            if (rndnumb == 5)
+            {
+                string weather = "Stormy";
+                return weather;
+            }
+            return "";
+            
+        } 
+
+
+
+    }
+}
